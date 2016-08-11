2cee04b27d358eb5ede54a1bd5d45f8580e1be5d
silvanagv <silvanagoberdhanvigle@gmail.com>
2016-08-11 14:10:13 -0400

    finished

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/hometown' do
+    "My hometown is Oxford."
+  end
+
+  get '/name' do
+    "My name is Silvana."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Gimme Shelter."
+  end
 end
